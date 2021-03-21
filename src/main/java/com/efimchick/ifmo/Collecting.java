98cf858f125bfebc530e4b48838368b1ad7afbf4
DzmitryTysevich <dztysevich@gmail.com>
2021-03-21 20:34:51 +0300

    final task with changes

--- src/main/java/com/efimchick/ifmo/Collecting.java
+++ src/main/java/com/efimchick/ifmo/Collecting.java
@@ -4,7 +4,6 @@ import com.efimchick.ifmo.util.CourseResult;
 import com.efimchick.ifmo.util.Person;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
